ecf082343c55215e9ebd9fbeb3f84a80d80c2aa4
Peeyush <69499613+PeeyushSachan@users.noreply.github.com>
2025-01-24 21:17:21 +0530

    code

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
 include('cdn.php');
 
-// Simple API example
+// Simple API exampleasdfasfasdfasdfasfsafasf
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     echo json_encode(["message" => "Welcome to the API!"]);
 }
 ?>
+
+
